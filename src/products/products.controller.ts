63c9adffc73a36d921cbb8ea6a91df36381329eb
robert <rhsu123@gmail.com>
2020-11-11 21:30:26 -0800

    ok I think I get it...

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 
 import { ProductsService } from './products.service';
 
@@ -16,6 +16,16 @@ export class ProductsController {
         const prodId = new Date().toString();
         const generatedId = this.productsService.insertProduct(title, description, price);
         return { id: generatedId }
+    }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string) {
+        return this.productsService.getSingleProduct(prodId);
     }
 }
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-    products: Product[] = [];
+    private products: Product[] = [];
 
     insertProduct(title: string, description: string, price: number) {
-        const prodId = new Date().toString();
+        // const prodId = new Date().toString();
+        const prodId = Math.random().toString();
         const newProduct = new Product(prodId, title, description, price);
         this.products.push(newProduct);
         return prodId;
+    }
+
+    getProducts() {
+        return [...this.products];
+    }
+
+    getSingleProduct(productId: string) {
+        const product = this.products.find((prod) => prod.id == productId)
+        if (!product) {
+            throw new NotFoundException('could not find product');
+        }
+        return {...product };
     }
 }
